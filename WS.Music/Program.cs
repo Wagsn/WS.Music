fac2c37acc59284234859b983ef971ae93fa537a
wangsen <wagsn@foxmail.com>
2019-04-02 09:44:33 +0800

    添加项目文件。

---
+++ WS.Music/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WS.Music
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
