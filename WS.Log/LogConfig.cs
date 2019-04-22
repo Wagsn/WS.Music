34252dc36ab682e589cb6f49ecf74a2bf2e8680c
wangsen <wagsn@foxmail.com>
2019-04-22 22:39:29 +0800

    引入Layui框架

--- WS.Log/LogConfig.cs
+++ WS.Log/LogConfig.cs
@@ -11,8 +11,6 @@ namespace WS.Log
     /// </summary
     public class LogConfig
     {
-        public LogConfig() { }
-        
         /// <summary>
         /// 是否输出日志
         /// </summary>
@@ -75,6 +73,7 @@ namespace WS.Log
         /// </summary>
         public string MaxFileSize { get; set; }
 
+
         /// <summary>
         /// 档案文件模板
         /// </summary>
@@ -99,58 +98,5 @@ namespace WS.Log
         /// 档案文件是否压缩
         /// </summary>
         public bool? ZipArchiveFile { get; set; }
-    }
-    /// <summary>
-    /// 单独一个日志器的配置
-    /// </summary>
-    public class LoggerConfig2
-    { 
-
-        /// <summary>
-        /// 错误日志文件输出路径模板（"./log/${LoggerName}/${Date}.log"）
-        /// </summary>
-        public string LogOutTemplate { get; set; }
-
-        /// <summary>
-        /// 错误日志文件输出路径模板（"./log/${LoggerName}/error/${Date}.log"）
-        /// </summary>
-        public string ErrOutTemplate { get; set; }
-
-        /// <summary>
-        /// 日志文件名模板（"${Year} ${Month} ${Day}.log"）
-        /// 暂时支持标签（LoggerName：TodoContriller，Date：yyy-MM-dd）
-        /// </summary>
-        public string FileNameTemplate { get; set; }
-
-        /// <summary>
-        /// 日志项模板
-        /// "[${DateTime}] [${LoggerLevel}] [${LoggerName}] ${Message}" -> "[2018-11-18 17:15.452154+8:00] [Trace] [TodoController] Message"
-        /// </summary>
-        public string LogItemTemplate { get; set; }
-
-        /// <summary>
-        /// 日期格式（yyyy-MM-dd）
-        /// </summary>
-        public string DateFormat { get; set; }
-
-        /// <summary>
-        /// 时间格式（09:53:12.154451+8:00）
-        /// </summary>
-        public string TimeFormat { get; set; }
-
-        /// <summary>
-        /// 映射，键值对
-        /// 占位符与实际值的映射（LoggerName：TodoContriller）
-        /// 不在JSON中映射
-        /// TODO：LoggerName可能只需要赋值一次，但是DateTime是每次打印都在刷新的，将object改造成委托，采用动态计算机制 KVs[key](entity):string
-        /// </summary>
-        [JsonIgnore]
-        public Dictionary<string, object> Map = new Dictionary<string, object>();
-
-        /// <summary>
-        /// 通过LogEntity动态计算LogItem
-        /// </summary>
-        [JsonIgnore]
-        public Dictionary<string, Func<object, string>> DynanicMap { get; set; }
     }
 }
--- WS.Music/wwwroot/js/site.js
+++ WS.Music/wwwroot/js/site.js
@@ -2,3 +2,83 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Get|Set from Html, Save|Load from Storage|Server
+
+// 获取搜索表单数据
+function getSearchData() {
+    return {
+        pageIndex: $('[name=pageIndex]').val().trim(),
+        pageSize: $('[name=pageSize]').val().trim(),
+        keyWord: $('[name=keyWord]').val().trim(),
+    }
+}
+// 设置搜索表单数据  组件化与插件化
+function setSearchData(data) {
+    pageIndex: $('[name=pageIndex]').val(data.pageIndex)
+    pageSize: $('[name=pageSize]').val(data.pageSize)
+    keyWord: $('[name=keyWord]').val(data.keyWord)
+}
+// 设置歌曲列表数据
+function setSongList(data) {
+    //console.log("song list set data:", data)
+    let songList = $('.song-list-body')
+    let bodyHtml = ""
+    data.forEach(v => {
+        //console.log('song list item data: ', v)
+        let rowHtml = ""
+        rowHtml += '<tr>'
+        rowHtml += '<td>' + v.name + '</td>'
+        rowHtml += '<td>' + v.description + '</td>'
+        rowHtml += '<td>' + v.duration + '</td>'
+        rowHtml += '<td>' + v.releaseTime + '</td>'
+        rowHtml += '<td>' + v.url + '</td>'
+        rowHtml += '<td>' + '<a class="song-details-jump" >详情</a>' + '<a class="song-edit-jump" >编辑</a>' + '<a class="song-delete-jump" >删除</a>' + '</td>'
+        rowHtml += '</tr>'
+        bodyHtml += rowHtml;
+    })
+    songList.html(bodyHtml);
+}
+// 加载歌曲列表数据 query:{pageIndex: 0, pageSize: 10, keyWord: ""} loadSongList({}, setSongList)
+function loadSongList(query = {}) {
+    //console.log("song list load query:", query)
+    var request = {
+        pageIndex: query.pageIndex||0,
+        pageSize: query.pageSize||10,
+        keyWord: query.keyWord||""
+    }
+    //console.log("song list load request:", request)
+    $.ajax({
+        url: "../api/song/list/search",
+        method: "post",
+        contentType: "application/json",
+        data: JSON.stringify(request),
+        success: function (body) {
+            //console.log("song list body:", body)
+            if (body.code == "0") {
+                setSongList(body.data);
+            }
+            else {
+                alert('歌曲数据加载失败！')
+            }
+        },
+        error: function (data) {
+            alert('请求失败！' + JSON.stringify(data))
+        }
+    })
+}
+
+// 歌曲搜索请求
+function songSearch() {
+    //console.log('song search request:', getSearchData())
+    loadSongList(getSearchData())
+}
+
+// 分页查询请求
+class PageQuery {
+    constructor() {
+        this.pageIndex = 0
+        this.pageSize = 10
+        this.keyWord = ""
+    }
+}
