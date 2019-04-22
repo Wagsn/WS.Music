1bf78849efca02737847109cdbf70e87e18c8b6a
wangsen <wagsn@foxmail.com>
2019-04-22 17:45:54 +0800

    添加类型注释

--- WS.Core/Defines/StringExtension.cs
+++ WS.Core/Defines/StringExtension.cs
@@ -4,10 +4,13 @@ using System.Text;
 
 namespace WS.Core
 {
+    /// <summary>
+    /// 通用字符串扩展
+    /// </summary>
     public static class StringExtension
     {
         /// <summary>
-        /// 占位符替换，默认 {{KEY}}，自定义 ${KEY}
+        /// 占位符替换，默认 {{ KEY }}，自定义 ${ KEY }
         /// </summary>
         /// <param name="pairs"></param>
         /// <returns></returns>
