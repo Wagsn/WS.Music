d410d42cb2d188667bdbe5fbc9892d01756ca739
wangsen <wagsn@foxmail.com>
2019-04-23 22:46:00 +0800

    添加存储

--- WS.Music.Storage/IStore.cs
+++ WS.Music.Storage/IStore.cs
@@ -1,14 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace WS.Music.Stores
 {
     public interface IStore
     {
         /// <summary>
-        /// 数据集
+        /// 查询所有
         /// </summary>
         /// <typeparam name="TEntity"></typeparam>
         /// <returns></returns>
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        /// <summary>
+        /// 按条件查询
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="predicate">条件表达式</param>
+        /// <returns></returns>
+        IQueryable<TEntity> Find<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
+
+        /// <summary>
+        /// 添加所有
+        /// </summary>
+        /// <param name="entities"></param>
+        void AddAll(params object[] entities);
+
+        /// <summary>
+        /// 更新所有
+        /// </summary>
+        /// <param name="entities"></param>
+        void UpdateAll(params object[] entities);
+
+        /// <summary>
+        /// 删除所有
+        /// </summary>
+        /// <param name="entities"></param>
+        void DeleteAll(params object[] entities);
     }
 }
--- WS.Music.Storage/MusicStore.cs
+++ WS.Music.Storage/MusicStore.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using WS.Music.Entities;
@@ -23,6 +25,29 @@ namespace WS.Music.Stores
         public DbSet<TEntity> Set<TEntity>() where TEntity : class
         {
             return Context.Set<TEntity>();
+        }
+
+        public void DeleteAll(params object[] entities)
+        {
+            Context.RemoveRange(entities);
+            Context.SaveChanges();
+        }
+
+        public IQueryable<TEntity> Find<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public void AddAll(params object[] entities)
+        {
+            Context.AddRange(entities);
+            Context.SaveChanges();
+        }
+
+        public void UpdateAll(params object[] entities)
+        {
+            Context.UpdateRange(entities);
+            Context.SaveChanges();
         }
     }
 }
